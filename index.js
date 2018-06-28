eb31cbc1306474b7386ac41441b630e0709b3aa8
Matt Binns <mattbinns1@gmail.com>
2018-06-28 22:04:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {return string.toUpperCase()}
+function whisper(string) {return string.toLowerCase()}
+function sayHiToGrandma(string) {return "I can't hear you!"}
+function logShout(string) {console.log(string.toUpperCase())}
+function logWhisper(string) {console.log(string.toLowerCase())}
+function sayHiToGrandma(string) {return "YES INDEED!"}
+
+
+
