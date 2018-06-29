8ed6c29bef9627ed68210addbc15cd860683bfe6
Matt Binns <mattbinns1@gmail.com>
2018-06-29 00:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
-function sayHiToGrandma(string) {return "I can't hear you!"}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
+function sayHiToGrandma(string) {return "I can't hear you!"}
 function sayHiToGrandma(string) {return "YES INDEED!"}
 function sayHiToGrandma(string) {return "I love you, too."}
 
